3f083524f5acaf47d86dcadd0c8fbd72edd0b547
lcspaiva87 <lcspaiva87@gmail.com>
2022-02-12 19:17:09 -0300

    Modulo-Back-edd no front-end

--- src/pages/api/_lib/menageSubscription.ts
+++ src/pages/api/_lib/menageSubscription.ts
@@ -1,37 +1,41 @@
-import { query as q } from 'faunadb'
-import { fauna } from "../../../services/fauna"
-import { stripe } from '../../../services/stripe'
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+import { stripe } from "../../../services/stripe";
 
 export async function saveSubscription(
-    subscriptionId: string,
-    customerId: string
+  subscriptionId: string,
+  customerId: string,
+  createActtion = false
 ) {
-    //buscar user no DB com customer Id{customerID}
-    //salvar os dados da subscription no FaunaDB
-    const userRef = await fauna.query(
-        q.Select(
-            "ref",
-            q.Get(
-                q.Match(
-                    q.Index('user_by_stripe_customer_id'),
-                    customerId
-                )
-            )
-        )
+  //buscar user no DB com customer Id{customerID}
+  //salvar os dados da subscription no FaunaDB
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
     )
+  );
 
-    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
-    const subscriptionData ={
-        id:subscription.id,
-        userId: userRef,
-        status: subscription.status,
-        price_id:subscription.items.data[0].id
-    }
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].id,
+  };
+  if (createActtion) {
     await fauna.query(
-        q.Create(
-            q.Collection('subscriptions'),
-            { data: subscriptionData }
-        )
-    )
-
+      q.Create(q.Collection("subscriptions"), { data: subscriptionData })
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(q.Match(q.Index("subscription_by_id"), subscriptionId))
+        ),
+        { data: subscriptionData }
+      )
+    );
+  }
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -19,7 +19,11 @@ export const config = {
     bodyParser: false,
   },
 };
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -41,23 +45,32 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
-          case 'checkout.session.completed':
-
-          const checkoutSession = event.data.object as Stripe.Checkout.Session
-          await saveSubscription(
-            checkoutSession.subscription.toString(),
-            checkoutSession.customer.toString()
-          )
-            break
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+            break;
+          case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
+            break;
 
           default:
-            throw new Error('Unhandle event')
+            throw new Error("Unhandle event");
         }
       } catch (err) {
-        console.log("Erro",err)
-        return res.json({err:'Webhooks handler filed'})
+        console.log("Erro", err);
+        return res.json({ err: "Webhooks handler filed" });
       }
-
     }
     res.json({ received: true });
   } else {
